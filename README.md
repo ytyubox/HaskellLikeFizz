8bf076f1c04d591b284a3645c20faaf42478f939
Tsungyu Yu <ytyubox@gmail.com>
2020-09-20 22:55:07 +0800

    pair and list ok

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # HaskellLikeFizz
 
 A description of this package.
+
+1. No Loops
+2. No ifs 
+3. Function is Single Return
+4. No side-effects 
+5. No Assignments in Function
+6. No arrays
+7. Only Functions with 0 or 1 arguments
--- Tests/HaskellLikeFizzTests/HaskellLikeFizzTests.swift
+++ Tests/HaskellLikeFizzTests/HaskellLikeFizzTests.swift
@@ -2,14 +2,53 @@ import XCTest
 @testable import HaskellLikeFizz
 
 final class HaskellLikeFizzTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(HaskellLikeFizz().text, "Hello, World!")
+    func testadd() {
+        let result = HaskellAdd(1)(2)
+        XCTAssertEqual(3, result)
     }
-
+    func testPair() {
+        XCTAssertEqual(fst(pair(10)(20)), 10)
+        XCTAssertEqual(snd(pair(10)(20)), 20)
+        XCTAssertEqual(fst(pair(10)(nil)), 10)
+        XCTAssertEqual(snd(pair(10)(nil)), nil)
+    }
+    
     static var allTests = [
-        ("testExample", testExample),
+        ("testadd", testadd),
     ]
 }
+
+struct HaskellAdd {
+    internal init(_ a: Int) {
+        self.a = a
+    }
+    
+    private let a:Int
+    func callAsFunction(_ b: Int) -> Int {
+        a + b
+    }
+}
+func pair(_ first:Int) -> (Int?) -> list {
+    return {
+        list(first, $0)
+    }
+}
+func fst(_ list: list) -> Int {
+    list.first
+}
+func snd(_ list: list) -> Int? {
+    list.second
+}
+struct list {
+    internal init(
+        _ first: Int,
+        _ second: Int?) {
+        self.first = first
+        self.second = second
+    }
+    
+    let first:Int
+    let second:Int?
+}
+
+
